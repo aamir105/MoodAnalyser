13b41074fbe36561ae28477b6e0bed9f45aafa00
aamir105 <aamiranwer2018@gmail.com>
2024-08-06 15:22:23 +0530

    Added:UC1_Happy_Sad_Mood

--- src/main/java/org/example/MoodAnalyzer.java
+++ src/main/java/org/example/MoodAnalyzer.java
@@ -1,9 +1,20 @@
 package org.example;
 
 public class MoodAnalyzer {
+    public String analyseMood(String message){
+        message = message.toLowerCase();
+        if (message.contains("sad")){
+            return "SAD";
+        } else if (message.contains("happy")) {
+            return "HAPPY";
+        }else{
+            return " ";
+        }
+    }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to mood analyser test.");
+//        System.out.println("Welcome to mood analyser test.");
+
 
     }
 }
